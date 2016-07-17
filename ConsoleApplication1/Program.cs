d26a3c7420ae3e0a623d65090f04d50417a14497
https://github.com/csyangpu1983 <15873115257@qq.com>
2016-07-17 18:22:28 +0800

    2016/07/17代码分享
    2016/07/17代码分享

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+
+////看到这个屏幕的同学请在群里跟我发消息,谢谢
+            //Print(0);
+
+            //Console.WriteLine("hello world");
+
+            #endregion
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("{0}*{1}={2}\t",j,i,i*j);
+                }
+                Console.Write("\n");
+            }
+            
+        }
+
+
+        //static void Print(int i)
+        //{
+        //    Console.WriteLine(i);
+        //    if(i<3)
+        //    {
+        //        Print(++i);
+        //    }
+        //    Console.WriteLine(i);
+        //}
+    }
+}
